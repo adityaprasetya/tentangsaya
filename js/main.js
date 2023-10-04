ead7f3bde06327150e0e61fc8a81b54075331685
adityaprasetya <adityaprasetya82@gmail.com>
2023-10-04 10:34:32 +0700

    Update main.js
    perbaikan js navbar

--- js/main.js
+++ js/main.js
@@ -17,21 +17,21 @@
 
 
     // Fixed Navbar
-    // $(window).scroll(function () {
-    //     if ($(window).width() < 992) {
-    //         if ($(this).scrollTop() > 45) {
-    //             $('.fixed-top').addClass('bg-white shadow');
-    //         } else {
-    //             $('.fixed-top').removeClass('bg-white shadow');
-    //         }
-    //     } else {
-    //         if ($(this).scrollTop() > 45) {
-    //             $('.fixed-top').addClass('bg-white shadow').css('top', 0);
-    //         } else {
-    //             $('.fixed-top').removeClass('bg-white shadow').css('top', -50);
-    //         }
-    //     }
-    // });
+    $(window).scroll(function () {
+        if ($(window).width() < 992) {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-white shadow');
+            } else {
+                $('.fixed-top').removeClass('bg-white shadow');
+            }
+        } else {
+            if ($(this).scrollTop() > 50) {
+                $('.fixed-top').addClass('bg-white shadow').css('top', 0);
+            } else {
+                $('.fixed-top').removeClass('bg-white shadow').css('top', -50);
+            }
+        }
+    });
     
     
     // Back to top button
